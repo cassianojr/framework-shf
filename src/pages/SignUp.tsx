57589233c8fbbeabe0a3c176e043b742c144ad06
cassianojr <cassiano.jr.2009@hotmail.com>
2023-09-22 13:27:53 -0300

    Feature: Add new ECOS modal and forms improvements

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -64,7 +64,7 @@ export default function SignUp() {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
@@ -96,6 +96,7 @@ export default function SignUp() {
                   label="Password"
                   type="password"
                   id="password"
+                  inputProps={{"minLength":6}}
                   autoComplete="new-password"
                 />
               </Grid>
