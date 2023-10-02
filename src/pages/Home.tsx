7208a5554527925a0db29f9cb70d0bb3f6123195
cassianojr <cassiano.jr.2009@hotmail.com>
2023-10-02 15:29:38 -0300

    Translated home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,9 +9,9 @@ function Home() {
 
   const heroData = {
     title: 'Explore fatores sociais e humanos',
-    description: 'The framework SHFiRM-SECO is a tool to help the identification and improvement of social and human factors in requirements management in software ecosystems.',
+    description: 'O framework SHFiRM-SECO é uma ferramenta para ajudar a identificação e a melhoria dos fatores sociais e humanos em gerência de requisitos em ecossistemas de software.',
     image,
-    button: 'Try now!',
+    button: 'Teste agora!',
     buttonLink: '/framework'
   }
 
@@ -24,21 +24,21 @@ function Home() {
         <section id="about">
           <Box sx={{ my: 4 }}>
             <Typography variant="h4" component="h1" gutterBottom>
-              ABOUT the Framework
+              SOBRE o Framework
             </Typography>
             <Typography variant="h5" component="h2" gutterBottom sx={{ textAlign: 'justify' }}>
-              Identifying and improving social and human factors in requirements management in software ecosystems is challenging. The Framework SHFiRM-SECO supports identifying and improving social and human factors in requirements management in software ecosystems.
+              Identificar e melhorar fatores sociais e humanos em gerência de requisitos em ecossistemas de software é desafiador. O framework SHFiRM-SECO auxilia a identificar e melhorar os fatores humanos e sociais em gerência de requisitos em ecossistemas de software.
             </Typography>
 
             <Typography variant="h4" component="h1" gutterBottom>
-              Get a 360 view of social and human factors
+              Tenha uma visão 360 dos fatores sociais e humanos
             </Typography>
             <Typography variant="h5" component="h2" gutterBottom sx={{ textAlign: 'justify' }}>
               <ol>
-                <li>Identify the social and human factors that influence requirements management activities in your software ecosystem; </li>
-                <li>Identify the contextual characteristics of requirements management in your software ecosystem;</li>
-                <li>Point out the barriers you face to improving social and human factors;</li>
-                <li>Get suggestions for strategies and coping mechanisms to use for requirements management in your software ecosystem;</li>
+                <li>Identifique os fatores sociais e humanos que influenciam as atividades de gerência de requisitos em seu ecossistema de software;</li>
+                <li>Identifique as características contextuais da gerência de requisitos em seu ecossistema de software;</li>
+                <li>Aponte as barreiras que você enfrenta para melhorar os fatores sociais e humanos;</li>
+                <li>Receba sugestões para estratégias e mecanismos de enfrentamento para usar na gerência de requisitos em seu ecossistema de software;</li>
               </ol>
             </Typography>
             <Button
@@ -46,7 +46,7 @@ function Home() {
               to={heroData.buttonLink}
               variant="contained"
               sx={{ mt: 3, mr: 2 }}>
-              Try now!
+              Teste agora!
             </Button>
           </Box>
         </section>
