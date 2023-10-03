b665026f2a3c6da70ba93c210db3ffc4494c021f
cassianojr <cassiano.jr.2009@hotmail.com>
2023-10-03 09:30:44 -0300

    Translated guidelines page

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,12 @@ import { I18nextProvider } from 'react-i18next';
 import home_en from "./translations/en/home.json";
 import menu_en from "./translations/en/menu.json";
 import common_en from './translations/en/common.json';
+import guidelines_en from './translations/en/guidelines.json';
 
 import home_pt_br from './translations/pt_br/home.json';
 import menu_pt_br from './translations/pt_br/menu.json';
 import common_pt_br from './translations/pt_br/common.json';
-
+import guidelines_pt_br from './translations/pt_br/guidelines.json';
 
 i18next.init({
   interpolation: { escapeValue: false }, 
@@ -20,12 +21,14 @@ i18next.init({
     en: {
       home: home_en,
       menu: menu_en,
-      common: common_en
+      common: common_en,
+      guidelines: guidelines_en
     },
     pt_br: {
       home: home_pt_br,
       menu: menu_pt_br,
-      common: common_pt_br
+      common: common_pt_br,
+      guidelines: guidelines_pt_br
     },
   },
 })
--- src/pages/Guidelines.tsx
+++ src/pages/Guidelines.tsx
@@ -1,8 +1,10 @@
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import { Box, Container, Toolbar, Typography } from '@mui/material';
+import { useTranslation } from "react-i18next";
 
 function Guidelines() {
+  const { t } = useTranslation('guidelines');
 
   const typographyStyle = {
     textAlign: 'justify',
@@ -15,19 +17,19 @@ function Guidelines() {
       <Navbar />
       <Toolbar />
       <Container sx={{ minHeight: '70vh' }}>
-        <section id="about">
+        <section id="guidelines">
           <Box sx={{ my: 4 }}>
             <Typography variant="h4" component="h1" gutterBottom>
-              Diretrizes para Utilização
+              {t('title')}
             </Typography>
             <Typography sx={typographyStyle}>
-              <i style={{fontWeight:'bold'}}>Pergunte (Ask):</i> A primeira etapa é identificar os problemas. O primeiro passo para avaliar os FSH que influenciam a gerência de requisitos em ECOS é conhecer os fatores do dia a dia dos profissionais que atuam nas atividades da gerência de requisitos. Com isso, pergunte aos profissionais da equipe quais fatores eles já enfrentaram, apresente a lista de barreiras e entenda se eles já encontraram algumas delas em suas atividades da gerência de requisitos em ECOS. É fundamental que esta etapa seja feita individualmente, podendo também utilizar formulários on-line se preferir.
+              <i style={{fontWeight:'bold'}}>{t('list.1.title')}</i> {t('list.1.description')}
             </Typography>
             <Typography sx={typographyStyle}>
-              <i style={{fontWeight:'bold'}}>Planeje (Plan):</i> As estratégias de melhoria devem ocorrer no nível individual da equipe e organizacional, dependendo dos fatores e barreiras identificados. Após coletar feedback, as respostas devem ser avaliadas para determinar quais barrerias e fatores requerem a priorização para melhoria. Deve-se planejar estratégias de melhoria e alocar recursos financeiros para retificação, se necessário.
+              <i style={{fontWeight:'bold'}}>{t('list.2.title')}</i>{t('list.2.description')}
               </Typography>
             <Typography sx={typographyStyle}>
-              <i style={{fontWeight:'bold'}}>Execute (Act):</i> Pequenas melhorias contínuas e iterativas são fundamentais. Implemente gradualmente as estratégias de melhoria e avalie se elas efetivamente abordam as barreiras e os fatores. Acompanhar o sucesso das estratégias é fundamental para superar as barreiras e melhorar os FSH. Depos que as melhorias planejadas forem implementadas, repita esse processo de três etapas iterativamente para sustentar o progresso e abordar os seguintes fatores e barreiras, mantendo assim um processo de melhoria contínua.
+              <i style={{fontWeight:'bold'}}>{t('list.3.title')}</i>{t('list.3.description')}
               </Typography>
 
           </Box>
