1a5683134099c2c45aa5fb64027d35b20a633411
cassianojr <cassiano.jr.2009@hotmail.com>
2024-02-16 11:34:03 -0300

    Feat: added tabs on EcosDashboard with the interpretation of consensus

--- src/components/EcosDashboard/ResultDataDisplay.tsx
+++ src/components/EcosDashboard/ResultDataDisplay.tsx
@@ -1,9 +1,10 @@
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
-import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+import { Accordion, AccordionDetails, AccordionSummary, Tab, Tabs } from '@mui/material';
 import i18next from 'i18next';
-import { Framework, FrameworkItem } from '../../types/Framework.type';
+import { Framework } from '../../types/Framework.type';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { ProgressBar } from './DataGridProgressBar';
+import ResultDataGrid from '../ResultDataGrid/ResultDataGrid';
+import { Box } from '@mui/system';
+import React from 'react';
 
 
 interface ResultDataDisplayProps {
@@ -12,124 +13,38 @@ interface ResultDataDisplayProps {
 }
 
 export default function ResultDataDisplay({ frameworkComponent, expanded = false }: ResultDataDisplayProps) {
+  const [tabValue, setTabValue] = React.useState(0);
 
-  // 5 - concordo totalmente    1 33%
-  // 4 - concordo              1 33%
-
-
-  // 3 - nem concordo nem discordo 0
-  // 2 - discordo              1 33%
-  // 1 - discordo totalmente   0
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  }
 
 
-  const columns = [
-    {
-      field: 'name',
-      headerName: 'Nome',
-      width: 545,
-      sortable: false,
-      resizable: false,
-      renderCell: (params: GridRenderCellParams<FrameworkItem, number>) => <Typography>{params.value}</Typography>,
-      valueGetter: (params: GridRenderCellParams<FrameworkItem, number>) => params.row.names[i18next.language]
-    },
-    {
-      field: 'totallyDisagree',
-      headerName: 'Discordo Totalmente',
-      width: 100,
-      sortable: false,
-      resizable: false,
-      renderCell: (params: GridRenderCellParams<FrameworkItem, number>) => <ProgressBar value={Number(params.value)} />,
-      valueGetter: (params: GridRenderCellParams<FrameworkItem, number>) => {
-        const disagree = params.row.disagree ?? 0;
-        const totallyDisagree = params.row.totallyDisagree ?? 0;
-        const totallyAgree = params.row.totallyAgree ?? 0;
-        const agree = params.row.agree ?? 0;
-        const neutral = params.row.neutral ?? 0;
+  interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+  }
+  const TabPanel = (props: TabPanelProps) => {
+    const { children, value, index, ...other } = props;
 
-        const totalVotes = agree+disagree+totallyDisagree+totallyAgree+neutral;
-        
-        return totallyDisagree / totalVotes;
-      }
-    },
-    {
-      field: 'disagree',
-      headerName: 'Discordo',
-      width: 100,
-      sortable: false,
-      resizable: false,
-      renderCell: (params: GridRenderCellParams<FrameworkItem, number>) => <ProgressBar value={Number(params.value)} />,
-      valueGetter: (params: GridRenderCellParams<FrameworkItem, number>) => {
-        const disagree = params.row.disagree ?? 0;
-        const totallyDisagree = params.row.totallyDisagree ?? 0;
-        const totallyAgree = params.row.totallyAgree ?? 0;
-        const agree = params.row.agree ?? 0;
-        const neutral = params.row.neutral ?? 0;
-
-        const totalVotes = agree+disagree+totallyDisagree+totallyAgree+neutral;
-        
-        return disagree / totalVotes;
-      }
-    },
-    {
-      field: 'neutral',
-      headerName: 'Não concordo nem discordo',
-      width: 120,
-      sortable: false,
-      resizable: false,
-      renderCell: (params: GridRenderCellParams<FrameworkItem, number>) => <ProgressBar value={Number(params.value)} />,
-      valueGetter: (params: GridRenderCellParams<FrameworkItem, number>) => {
-        const disagree = params.row.disagree ?? 0;
-        const totallyDisagree = params.row.totallyDisagree ?? 0;
-        const totallyAgree = params.row.totallyAgree ?? 0;
-        const agree = params.row.agree ?? 0;
-        const neutral = params.row.neutral ?? 0;
-
-        const totalVotes = agree+disagree+totallyDisagree+totallyAgree+neutral;
-        
-        return neutral / totalVotes;
-      }
-    },
-    {
-      field: 'agree',
-      headerName: 'Concordo',
-      width: 100,
-      sortable: false,
-      resizable: false,
-      renderCell: (params: GridRenderCellParams<FrameworkItem, number>) => <ProgressBar value={Number(params.value)} />,
-      valueGetter: (params: GridRenderCellParams<FrameworkItem, number>) =>{
-        const totallyDisagree = params.row.totallyDisagree ?? 0;
-        const disagree = params.row.disagree ?? 0;
-        const neutral = params.row.neutral ?? 0;
-        const agree = params.row.agree ?? 0;
-        const totallyAgree = params.row.totallyAgree ?? 0;
-
-        const totalVotes = agree+disagree+totallyDisagree+totallyAgree+neutral;
-        
-        return agree / totalVotes;
-      }
-
-    },
-    {
-      field: 'totallyAgree',
-      headerName: 'Concordo Totalmente',
-      width: 100,
-      sortable: false,
-      resizable: false,
-      renderCell: (params: GridRenderCellParams<FrameworkItem, number>) => <ProgressBar value={Number(params.value)} />,
-      valueGetter: (params: GridRenderCellParams<FrameworkItem, number>) =>{
-        const totallyDisagree = params.row.totallyDisagree ?? 0;
-        const disagree = params.row.disagree ?? 0;
-        const neutral = params.row.neutral ?? 0;
-        const agree = params.row.agree ?? 0;
-        const totallyAgree = params.row.totallyAgree ?? 0;
-
-        const totalVotes = agree+disagree+totallyDisagree+totallyAgree+neutral;
-        
-        return totallyAgree / totalVotes;
-      }
-
-    }
-  ]
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+       
+      >
+        {value === index && (
+          <Box  sx={{ overflow: 'auto', height: 300}}>
+            {children}
+          </Box>
+        )}
+      </div>
+    );
+  }
 
   return (
     <Accordion defaultExpanded={expanded}>
@@ -141,24 +56,23 @@ export default function ResultDataDisplay({ frameworkComponent, expanded = false
         {frameworkComponent?.labels[i18next.language]}
       </AccordionSummary>
       <AccordionDetails>
-        <div style={{ height: 300 }}>
-          <DataGrid
-            rows={frameworkComponent?.items ?? []}
-            columns={columns}
-            sx={{
-              '& .MuiDataGrid-columnHeaderTitle': {
-                textOverflow: "clip",
-                whiteSpace: "break-spaces",
-                lineHeight: '1.2rem'
-              }
-            }}
-            disableRowSelectionOnClick
-            disableColumnMenu
-            hideFooterPagination
-            hideFooter
-          />
-        </div>
+        <Box sx={{ height: 340 }}>
 
+          <Box >
+            <Tabs value={tabValue} onChange={handleTabChange} aria-label="data display tabs">
+              <Tab label="Escala Likert" value={0} />
+              <Tab label="Resultado" value={1} />
+            </Tabs>
+          </Box>
+
+          <TabPanel value={tabValue} index={0}>
+            <ResultDataGrid frameworkComponent={frameworkComponent} columnType='likert' />
+          </TabPanel>
+          <TabPanel value={tabValue} index={1}>
+          <ResultDataGrid frameworkComponent={frameworkComponent} columnType='result' />
+          </TabPanel>
+          
+        </Box>
       </AccordionDetails>
     </Accordion>
   )
