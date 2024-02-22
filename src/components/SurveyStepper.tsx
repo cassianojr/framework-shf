ea0b0d3243200d82ae769185ae856d0612c7e82a
cassianojr <cassiano.jr.2009@hotmail.com>
2024-02-22 17:12:38 -0300

    Changed ECOSSurvey base component to Stepper

---
+++ src/components/SurveyStepper.tsx
@@ -0,0 +1,225 @@
+import { Button, MobileStepper, Paper, Typography } from '@mui/material';
+import React from 'react'
+import { Framework, FrameworkItem } from '../types/Framework.type';
+import { Modal } from './Modal';
+import { Box } from '@mui/system';
+import { useTranslation } from 'react-i18next';
+import FrameworkComponent from './FrameworkComponent';
+import { QuestionService } from '../services/QuestionService';
+import { NewAnswers } from '../types/Answer.type';
+import { useNavigate } from 'react-router-dom';
+
+interface SurveyStepperProps {
+  stepsVote: {
+    id: string,
+    title: string,
+    items: React.MutableRefObject<FrameworkItem[]>,
+    changeItems: (value: FrameworkItem[]) => void,
+    order: number
+  }[],
+  frameworkItems: {
+    socialHumanFactors: Framework | undefined,
+    contextualCharacteristics: Framework | undefined,
+    barriersToImproving: Framework | undefined,
+    strategies: Framework | undefined,
+    copingMechanisms: Framework | undefined,
+    setSocialHumanFactors: (value: Framework) => void,
+    setContextualCharacteristics: (value: Framework) => void,
+    setBarriersToImproving: (value: Framework) => void,
+    setStrategies: (value: Framework) => void,
+    setCopingMechanisms: (value: Framework) => void
+  },
+  ecosId: string | undefined,
+  currentRound: number,
+  user_id: string,
+  user_email: string,
+  setFeedBackSnackBar: (value: { severity: "success" | "error", text: string }) => void,
+  setFeedBackSnackbarState: (value: boolean) => void
+}
+
+export default function SurveyStepper({ stepsVote, frameworkItems, ecosId, currentRound, user_id, user_email, setFeedBackSnackBar, setFeedBackSnackbarState }: SurveyStepperProps) {
+  const { t } = useTranslation('ecos_survey');
+  const navigate = useNavigate();
+
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [answers, setAnswers] = React.useState<NewAnswers | undefined>(undefined);
+
+
+  const setAnswersToFrameworkComponent = () => {
+    frameworkItems.setSocialHumanFactors({
+      ...frameworkItems.socialHumanFactors,
+      items: stepsVote.filter((item) => item.id === "social-human-factors")[0].items.current as FrameworkItem[]
+    } as Framework);
+
+    frameworkItems.setContextualCharacteristics({
+      ...frameworkItems.contextualCharacteristics,
+      items: stepsVote.filter((item) => item.id === "contextual-characteristics")[0].items.current as FrameworkItem[]
+    } as Framework);
+
+    frameworkItems.setBarriersToImproving({
+      ...frameworkItems.barriersToImproving,
+      items: stepsVote.filter((item) => item.id === "barriers-to-improving")[0].items.current as FrameworkItem[]
+    } as Framework);
+
+    frameworkItems.setStrategies({
+      ...frameworkItems.strategies,
+      items: stepsVote.filter((item) => item.id === "strategies")[0].items.current as FrameworkItem[]
+    } as Framework);
+
+    frameworkItems.setCopingMechanisms({
+      ...frameworkItems.copingMechanisms,
+      items: stepsVote.filter((item) => item.id === "coping-mechanisms")[0].items.current as FrameworkItem[]
+    } as Framework);
+  }
+
+
+  const createAnswersObject = () => {
+    const answers = {
+      user_id,
+      user_email,
+      ecossystem_id: ecosId,
+      round: currentRound,
+      answers: stepsVote.map((stepVoteItem) => {
+        return {
+          framework_item: stepVoteItem.id,
+          question: stepVoteItem.title,
+          items: stepVoteItem.items.current.map((item) => {
+            return {
+              id: item.id,
+              ids: item.ids,
+              names: item.names,
+              answer: item.ratio
+            }
+          })
+        }
+      })
+    } as NewAnswers;
+
+    return answers;
+  }
+
+  const handleSaveAnswers = () => {
+    if (!answers) return;
+
+    QuestionService.saveAnswers(answers, () => {
+      setFeedBackSnackBar({ severity: "success", text: t('answers_saved') });
+      setFeedBackSnackbarState(true);
+
+      setTimeout(() => {
+        navigate('/dashboard');
+      }, 3000);
+
+    }, () => {
+      setFeedBackSnackBar({ severity: "error", text: t('answers_not_saved') });
+      setFeedBackSnackbarState(true);
+    });
+  }
+
+  const handleNext = () => {
+    if (activeStep === steps.length - 1) {
+      setAnswersToFrameworkComponent();
+      setAnswers(createAnswersObject());
+    }
+    if (activeStep === steps.length) {
+      handleSaveAnswers();
+      return;
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const stepperContents = stepsVote.map((step) => {
+    return {
+      id: step.id,
+      title: step.title,
+      items: step.items,
+      changeItems: step.changeItems,
+      order: step.order,
+      type: 'vote',
+      texts: [],
+      lists: [],
+      dataTable: <Modal.FrameworkDataTable key={step.id} items={step.items} changeItems={step.changeItems} />
+    }
+  });
+
+  const steps = [
+    {
+      title: t('welcome_text'),
+      texts: [
+        t('instructions.p1'),
+        t('instructions.p2')
+      ],
+      lists: [
+        t('instructions.i1'),
+        t('instructions.i2'),
+        t('instructions.i3'),
+        t('instructions.i4')
+      ],
+      dataTable: <></>,
+      type: 'text'
+    },
+    ...stepperContents
+  ];
+
+  return (
+    <Paper elevation={3} sx={{ padding: '2rem', width: (activeStep === steps.length) ? '85%' : '55%', margin: 'auto' }}>
+      <Box>
+        {activeStep === steps.length ? (
+          <Box>
+            <FrameworkComponent
+              copingMechanisms={frameworkItems.copingMechanisms}
+              contextualCharacteristics={frameworkItems.contextualCharacteristics}
+              socialHumanFactors={frameworkItems.socialHumanFactors}
+              barriersToImproving={frameworkItems.barriersToImproving}
+              strategies={frameworkItems.strategies}
+              showSuggestions={false}
+            />
+          </Box>)
+          : <></>}
+
+        {steps[activeStep] && (
+          <>
+            <Typography variant='h6'>{steps[activeStep]?.title ?? ''}</Typography>
+
+            {(steps[activeStep].type === 'text' && activeStep < steps.length) ? (
+              <>
+                {steps[activeStep].texts.map((text, index) => (
+                  <Typography key={index} sx={{ textAlign: 'justify', marginBottom: '1rem', textIndent: '1rem' }}>{text}</Typography>
+                ))}
+                <ul>
+
+                  {steps[activeStep].lists.map((text, index) => (
+                    <li key={index}>{text}</li>
+                  ))}
+                </ul>
+              </>
+
+            ) : (
+              <Box height={'70vh'}>
+                {steps[activeStep]?.dataTable ?? <></>}
+              </Box>
+            )}
+          </>
+        )}
+      </Box>
+      <MobileStepper
+        variant="progress"
+        steps={steps.length + 1}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button variant='contained' onClick={handleNext}>{(activeStep !== steps.length) ? (activeStep !== steps.length - 1) ? t('next_btn') : t('view_answer_btn') : t('save_btn')}</Button>
+        }
+        backButton={
+          <Button variant='outlined' onClick={handleBack}>{t('back_btn')}</Button>
+        }
+      />
+    </Paper>
+  )
+}
+
--- src/pages/EcosSurvey.tsx
+++ src/pages/EcosSurvey.tsx
@@ -1,21 +1,19 @@
 import Navbar from "../components/Navbar";
-import { Button, Divider, Paper, Toolbar, Typography } from "@mui/material";
+import { Paper, Toolbar, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import Footer from "../components/Footer";
 import Box from '@mui/material/Box';
 import React from 'react';
 
-import FrameworkComponent from "../components/FrameworkComponent";
 import { FirebaseService } from "../services/FirebaseService";
 import { Framework, FrameworkItem } from "../types/Framework.type";
-import { Modal } from "../components/Modal";
 import { AuthenticationContext, AuthenticationContextType } from "../context/authenticationContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import EcosystemService from "../services/EcosystemService";
-import { NewAnswers } from "../types/Answer.type";
 import { QuestionService } from "../services/QuestionService";
 import SnackBarComponent from "../components/SnackBarComponent";
+import SurveyStepper from "../components/SurveyStepper";
 
 interface SelectItemsProps {
   id: string,
@@ -33,11 +31,11 @@ export default function EcosSurvey() {
   const [socialHumanFactors, setSocialHumanFactors] = React.useState<Framework | undefined>(undefined);
   const [barriersToImproving, setBarriersToImproving] = React.useState<Framework | undefined>(undefined);
   const [strategies, setStrategies] = React.useState<Framework | undefined>(undefined);
+
   const [modalContent, setModalContent] = React.useState<SelectItemsProps[]>([] as SelectItemsProps[]);
   const [currentRound, setCurrentRound] = React.useState<number>(0);
-  const [answers, setAnswers] = React.useState<NewAnswers | undefined>(undefined);
   const [feedBackSnackbarState, setFeedBackSnackbarState] = React.useState(false);
-  const [feedBackSnackBar, setFeedBackSnackBar] = React.useState({severity: "success", text: ""} as {severity: "success" | "info" | "warning" | "error", text: string} );
+  const [feedBackSnackBar, setFeedBackSnackBar] = React.useState({ severity: "success", text: "" } as { severity: "success" | "info" | "warning" | "error", text: string });
 
   const { t } = useTranslation('ecos_survey');
 
@@ -62,25 +60,6 @@ export default function EcosSurvey() {
 
   const ecosId = useParams().ecosId;
   const [ecosName, setEcosName] = React.useState<string>("");
-
-  const handleSaveAnswers = () => {
-    if (!answers) return;
-
-    QuestionService.saveAnswers(answers, () => {
-      setFeedBackSnackBar({severity: "success", text: t('answers_saved')});
-      setFeedBackSnackbarState(true);
-
-      setTimeout(() => {
-        navigate('/dashboard');
-      }, 3000);
-      
-    }, () => {
-      setFeedBackSnackBar({severity: "error", text: t('answers_not_saved')});
-      setFeedBackSnackbarState(true);
-
-    });
-
-  }
 
   React.useEffect(() => {
     if (loading) return;
@@ -127,6 +106,8 @@ export default function EcosSurvey() {
       setBarriersToImproving(barriersToImprovingLocal);
       setStrategies(strategiesLocal);
 
+
+
       setModalContent([
         {
           id: "social-human-factors",
@@ -145,7 +126,7 @@ export default function EcosSurvey() {
         {
           id: "contextual-characteristics",
           title: t('cc_affirmative'),
-          items: contextualCharacteristicsRef ,
+          items: contextualCharacteristicsRef,
           changeItems: changeContextualCharacteristicsRef,
           order: 3
         },
@@ -159,25 +140,24 @@ export default function EcosSurvey() {
         {
           id: "strategies",
           title: t('strategies_affirmative'),
-          items: strategiesRef ,
+          items: strategiesRef,
           changeItems: changeStrategiesRef,
           order: 5
         }
       ]);
-
     }
 
     getEcosData().then((ecosData) => {
       if (!ecosData) return;
 
       QuestionService.getAnswersByUserId(getUser().uid)
-      .then((answers) => {
-        if(answers.find((answer) => answer.ecossystem_id == ecosId)?.round == ecosData.current_round) {
-          alert("You have already answered this survey for this round. You will be redirected to the dashboard.");
-          navigate('/dashboard');
-          return;
-        }
-      });
+        .then((answers) => {
+          if(answers.find((answer) => answer.ecossystem_id == ecosId)?.round == ecosData.current_round) {
+            alert("You have already answered this survey for this round. You will be redirected to the dashboard.");
+            navigate('/dashboard');
+            return;
+          }
+        });
 
       setCurrentRound(ecosData.current_round);
       setEcosName(ecosData.organization_name);
@@ -201,135 +181,38 @@ export default function EcosSurvey() {
     setAppLoading(false);
   }, [setStrategies, setCopingMechanisms, setContextualCharacteristics, setSocialHumanFactors, setBarriersToImproving, setModalContent, loading, signed, navigate, t, ecosId, getUser]);
 
-  const [currentModal, setCurrentModal] = React.useState<number>(0);
-
-  const WelcomeModal = () => {
-
-    return (<Modal.Root state={currentModal == 0} id="0" title={t('welcome_text')} handleClose={() => false}>
-      <Modal.Text>
-        <Typography sx={{ textAlign: 'justify', marginBottom: '1rem', textIndent: '1rem' }}>
-          {t('instructions.p1')}
-        </Typography>
-        <Typography sx={{ textAlign: 'justify', marginBottom: '1rem', textIndent: '1rem' }} variant="h6">
-          {t('instructions.p2')}
-        </Typography>
-        <ul>
-          <li>{t('instructions.i1')}</li>
-          <li>{t('instructions.i2')}</li>
-          <li>{t('instructions.i3')}</li>
-          <li>{t('instructions.i4')}</li>
-        </ul>
-      </Modal.Text>
-      <Divider />
-      <Modal.Actions handleClose={() => setCurrentModal(1)}>
-        <Button onClick={() => setCurrentModal(1)} variant='contained'>{t('next_btn')}</Button>
-      </Modal.Actions>
-    </Modal.Root>);
-  }
-
-  const SelectItemsModal = (props: SelectItemsProps) => {
-
-
-    const setAnswersToFrameworkComponent = () => {
-      setSocialHumanFactors({
-        ...socialHumanFactors,
-        items: modalContent.filter((item) => item.id === "social-human-factors")[0].items.current as FrameworkItem[]
-      } as Framework);
-
-      setContextualCharacteristics({
-        ...contextualCharacteristics,
-        items: modalContent.filter((item) => item.id === "contextual-characteristics")[0].items.current as FrameworkItem[]
-      } as Framework);
-
-      setBarriersToImproving({
-        ...barriersToImproving,
-        items: modalContent.filter((item) => item.id === "barriers-to-improving")[0].items.current as FrameworkItem[]
-      } as Framework);
-
-      setStrategies({
-        ...strategies,
-        items: modalContent.filter((item) => item.id === "strategies")[0].items.current as FrameworkItem[]
-      } as Framework);
-
-      setCopingMechanisms({
-        ...copingMechanisms,
-        items: modalContent.filter((item) => item.id === "coping-mechanisms")[0].items.current as FrameworkItem[]
-      } as Framework);
-    }
-
-    const createAnswersObject = () => {
-      const answers = {
-        user_id: getUser().uid,
-        user_email: getUser().email,
-        ecossystem_id: ecosId,
-        round: currentRound,
-        answers: modalContent.map((modalContentItem) => {
-          return {
-            framework_item: modalContentItem.id,
-            question: modalContentItem.title,
-            items: modalContentItem.items.current.map((item) => {
-              return {
-                id: item.id,
-                ids: item.ids,
-                names: item.names,
-                answer: item.ratio
-              }
-            })
-          }
-        })
-      } as NewAnswers;
-
-      return answers;
-    }
-
-    const handleNextBtnClick = () => {
-      if (currentModal >= 5) {
-        setAnswers(createAnswersObject);
-        setAnswersToFrameworkComponent();
-      }
-
-      setCurrentModal((curr) => curr + 1);
-    }
-
-    return (
-      <Modal.Root state={currentModal == props.order} id={props.id.toString()} title={props.title} handleClose={() => false} size='md'>
-        <Modal.FrameworkDataTable items={props.items} changeItems={props.changeItems} />
-
-        <Divider />
-        <Modal.Actions handleClose={() => setCurrentModal((curr) => curr + 1)}>
-          <Button onClick={() => setCurrentModal((curr) => curr - 1)} variant='contained'>{t('back_btn')}</Button>
-          <Button onClick={handleNextBtnClick} variant='contained'>{(currentModal != 5) ? t('next_btn') : t('view_answer_btn')}</Button>
-        </Modal.Actions>
-      </Modal.Root>
-    );
-  }
 
   return (
     <>
-      {!appLoading && <WelcomeModal />}
-      {!appLoading && modalContent.map((item) => <SelectItemsModal id={item.id} title={item.title} items={item.items} changeItems={item.changeItems} key={item.id} order={item.order} />)}
-      <SnackBarComponent snackBarState={feedBackSnackbarState} setSnackBarState={setFeedBackSnackbarState} severity={feedBackSnackBar.severity} text={feedBackSnackBar.text}/>
-      <Box sx={{ backgroundColor: '#ebebeb' }}>
+      <SnackBarComponent snackBarState={feedBackSnackbarState} setSnackBarState={setFeedBackSnackbarState} severity={feedBackSnackBar.severity} text={feedBackSnackBar.text} />
+      <Box >
         <Navbar />
         <Toolbar />
-        <Container sx={{ minHeight: '100vh' }} component={Paper} elevation={3} style={{ paddingTop: '1%' }} maxWidth={false}>
+        <Container sx={{ minHeight: '100vh', background: '#f5f5f5' }} component={Paper} elevation={3} style={{ paddingTop: '1%' }} maxWidth={false}>
 
-          {!appLoading && (<Box sx={{ margin: 'auto', width: '50%', p: 2 }} component={Paper} elevation={2}>
-            <Typography variant='h4' sx={{ textAlign: 'center', marginBottom: '1rem' }}>{ecosName}</Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '1rem', width: '30%', margin: 'auto' }}>
-              <Button variant='outlined' onClick={() => setCurrentModal(1)}>Editar Respostas</Button>
-              <Button variant='contained' onClick={handleSaveAnswers}>Enviar</Button>
-            </Box>
-          </Box>)}
+          {!appLoading && <Typography variant='h4' sx={{ textAlign: 'center', marginBottom: '1rem' }}>{ecosName}</Typography>}
 
-          <FrameworkComponent
-            copingMechanisms={copingMechanisms}
-            contextualCharacteristics={contextualCharacteristics}
-            socialHumanFactors={socialHumanFactors}
-            barriersToImproving={barriersToImproving}
-            strategies={strategies}
-            showSuggestions={false}
-          />
+          {!appLoading && <SurveyStepper
+            stepsVote={modalContent}
+            frameworkItems={{
+              socialHumanFactors,
+              contextualCharacteristics,
+              barriersToImproving,
+              strategies,
+              copingMechanisms,
+              setSocialHumanFactors,
+              setContextualCharacteristics,
+              setBarriersToImproving,
+              setStrategies,
+              setCopingMechanisms
+            }}
+            ecosId={ecosId}
+            currentRound={currentRound}
+            user_id={getUser().uid}
+            user_email={getUser().email}
+            setFeedBackSnackBar={setFeedBackSnackBar}
+            setFeedBackSnackbarState={setFeedBackSnackbarState}
+          />}
         </Container>
         <Footer />
       </Box>
